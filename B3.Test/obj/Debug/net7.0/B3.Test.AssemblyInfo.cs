a2f74e032db1f4385607c3ae4fc9842d2e9dc52e
Rafael Fernandes da Cunha <rafa_fernandes_cunha@hotmail.com>
2024-09-12 09:53:45 -0300

    atualização

--- B3.Test/obj/Debug/net7.0/B3.Test.AssemblyInfo.cs
+++ B3.Test/obj/Debug/net7.0/B3.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("B3.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dca3e1187f09246d6541def789fa6e1c10d324a")]
 [assembly: System.Reflection.AssemblyProductAttribute("B3.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("B3.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
